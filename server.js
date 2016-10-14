9d5f495f0a4ee4b5b959d121d257b1005115c59f
shawnwollenberg <shawn@wallyweb.com>
2016-10-14 19:06:08 +0000

    RD2 Commit

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
+//install MongoDB on C9: https://community.c9.io/t/setting-up-mongodb/1717
 var express = require('express');
+var mongoose = require('mongoose');
+var xUser=require ('./model/urlmodel');
+
 var app = express();
+mongoose.connect('mongodb://localhost/my_database');
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/showall/', function (req, res) {
+  xUser.find(function (err, data) {
+    if (err) return console.error(err);
+    res.send(data);
+    console.log(data);
+  })
+});
+
+app.get('/addnew/', function (req, res) {
+  var doc = {
+    name: "Shawn Wollenberg4",
+    username: "shawnwollenberg4",
+    password: "test"
+  }
+  var user= new xUser(doc);
+  user.save(function(err){
+    if (err) return console.error(err);
+    res.send("new user added" + doc);
+  })
 });
 
 app.listen(8080, function () {
